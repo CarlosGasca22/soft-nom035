3ee76fbe9cc5eedcccbdeec88dd2d9cdcbfab6ba
IsayPortilla <ricardoportilla986@gmail.com>
2022-04-27 12:09:58 -0500

    template

---
+++ resources/views/template/pie.blade.php
@@ -0,0 +1,28 @@
+<footer>
+	<div >
+		<div class="columna_izquierda">
+			<h2 class="contacto">Información de contacto</h2>
+				<span class="texto">Instituto Tecnológico de Toluca, Metepec Estado de México</span><br>
+				<span class="texto">Carlos Alberto Gasca de Anda</span>	
+				<span class="link"><a href="#">cgascad@toluca.tecnm.mx</span></a>
+				<span class="texto">Ricardo Isay Portilla Torres</span>
+				<span class="link"><a href="#">rportillat@toluca.tecnm.mx</span></a>
+		</div>
+		<div class="columna_media">
+			<a href="#"><img src="img/logo_small_icon_only_inverted.png" class="img_logo_footer"></a>
+		</div>
+		<div class="columna_derecha">
+			<h2 class="redes_sociales">
+				¡Síguenos en nuestras redes sociales!
+			</h2>
+			<ul><a href="#"><img src="img/fb.png" class="img_redes_sociales"></a></ul>	
+			<ul><a href="#"><img src="img/tw.png" class="img_redes_sociales"></a></ul>
+			<ul><a href="#"><img src="img/ins.png" class="img_redes_sociales"></a></ul>
+		</div>
+		<div class="derechos">
+			<p>Todos los derechos reservados </p>
+		</div>
+	</div>	
+</footer>
+</body>
+</html>
